9ce6a2b32d37bc2eb8895cf95fb3d057c870bbef
blakbox23 <boudii30@gmail.com>
2021-07-03 10:06:48 +0300

    Add sorted opinions to homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,8 @@ class HomeController < ApplicationController
   
   before_action :authenticate_user!
   def index
+
+    @opinions = current_user.opinions.order_by_most_recent
+    @count = @opinions.count
   end
 end
--- app/models/opinion.rb
+++ app/models/opinion.rb
@@ -1,3 +1,6 @@
 class Opinion < ApplicationRecord
   belongs_to :user
+  
+  scope :order_by_most_recent, -> {order(created_at: :desc) }
+  
 end
