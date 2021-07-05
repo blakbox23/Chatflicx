8ffc02eea13830afc2a81602f2b286a48c9d8f56
blakbox23 <boudii30@gmail.com>
2021-07-05 23:36:53 +0300

    Add styling to userpage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,7 @@ class HomeController < ApplicationController
     @opinion = Opinion.new
     @opinions = current_user.timeline_opinions
     @count = @opinions.count
+    
   end
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  has_many :opinions
+  has_one_attached :photo
+  has_one_attached :coverimage
+  has_many :opinions, dependent: :destroy
   has_many :followings, foreign_key: 'follower_id'
   has_many :following_users, through: :followings, source: :followed
   has_many :inverse_followings, class_name: 'Following', foreign_key: 'followed_id'
   has_many :followers, through: :inverse_followings, source: :follower
-  has_many :votes
+  has_many :votes, dependent: :destroy
 
   def users_to_follow
     User.where.not(id: Array.wrap(following_users)).and(User.where.not(id: id))
