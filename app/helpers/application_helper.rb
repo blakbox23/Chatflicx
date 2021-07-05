733705338f0ef0aa4183cfe32e099939b29cfd66
blakbox23 <boudii30@gmail.com>
2021-07-06 02:19:02 +0300

    Remove logic from views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+ def check_nav_pic(current_user)
+    if current_user.photo.attached?
+    image_tag(current_user.photo, class: "border-rounded") 
+    else
+    image_tag("https://via.placeholder.com/120")   
+    end
+ end
 end
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,17 @@
 module HomeHelper
+    def opinion_check_pic(opinion)
+        if opinion.user.photo.attached?
+        image_tag(opinion.user.photo, class: "border-rounded") 
+        else
+        image_tag("https://via.placeholder.com/120")
+        end
+    end
+
+    def check_pic(follower)
+        if follower.photo.attached?
+          image_tag(follower.photo, class: "rounded-circle") 
+        else
+          image_tag("https://via.placeholder.com/120")
+       end
+    end
 end
