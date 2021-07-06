389217c3b801076ddb79fa9ebec983a9feebc5e1
blakbox23 <boudii30@gmail.com>
2021-07-06 16:43:45 +0300

    Remove votes logic from view

--- app/helpers/votes_helper.rb
+++ app/helpers/votes_helper.rb
@@ -1,9 +1,10 @@
 module VotesHelper
   def vote_btn(opinion)
     if opinion.voted?(current_user)
-      link_to 'Unvote', vote_path(id: opinion.id), method: :delete
+      # link_to 'Unvote', vote_path(id: opinion.id), method: :delete
+      link_to "<i class='bi bi-star-fill'></i>".html_safe, vote_path(id: opinion.id), method: :delete  
     else
-      link_to('vote', { controller: 'votes', action: 'create', op_id: opinion.id }, method: :post)
+      link_to("<i class='bi bi-star'></i>".html_safe,{ controller: 'votes', action: 'create', op_id: opinion.id }, method: :post)
 
     end
   end
