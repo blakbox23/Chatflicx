bf6afb7ccac04c98c98b24bda55bb1c11d2f5891
blakbox23 <boudii30@gmail.com>
2021-07-06 11:54:48 +0300

    Remove active-record queries from view

--- app/models/opinion.rb
+++ app/models/opinion.rb
@@ -6,5 +6,9 @@ class Opinion < ApplicationRecord
   has_many :votes
   
   scope :order_by_most_recent, -> {order(created_at: :desc) }
+
+  def voted?(user)
+    !votes.where(user_id: user.id).empty?
+  end
   
 end
